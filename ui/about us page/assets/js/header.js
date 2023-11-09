10a56f84c13238098e5ccf5e0d51fe294d1e69a4
Somani Keshari <somanikeshari8726@gmail.com>
2023-11-09 12:24:50 +0530

    bnhg

---
+++ ui/about us page/assets/js/header.js
@@ -0,0 +1,36 @@
+	var $menu = $("nav#menu").mmenu({
+		// "extensions": ["pagedim-black", "theme-dark"],
+		counters: true,
+		keyboardNavigation: {
+			enable: true,
+			enhance: true
+		},
+		navbar: {
+			title: 'MENU'
+		},
+		navbars: [{position:'bottom',content: ['<a href="#0">© 2022 RecruitNxt</a>']}]}, 
+		{
+		// configuration
+		clone: true,
+		classNames: {
+			fixedElements: {
+				fixed: "menu_fixed",
+				sticky: "sticky"
+			}
+		}
+	});
+	var $icon = $("#hamburger");
+	var API = $menu.data("mmenu");
+	$icon.on("click", function () {
+		API.open();
+	});
+	API.bind("open:finish", function () {
+		setTimeout(function () {
+			$icon.addClass("is-active");
+		}, 100);
+	});
+	API.bind("close:finish", function () {
+		setTimeout(function () {
+			$icon.removeClass("is-active");
+		}, 100);
+	});
